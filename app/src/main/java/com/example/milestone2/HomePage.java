c15fded0f7995abacc276643d1649038121fec9c
April <aprilc1206@gmail.com>
2019-11-11 20:09:12 -0800

    Stylized Home Screen

--- app/src/main/java/com/example/milestone2/HomePage.java
+++ app/src/main/java/com/example/milestone2/HomePage.java
@@ -5,15 +5,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements View.OnClickListener{
 
     private TextView usernameTextView;
+    private Button findStationButton;
+    private ImageView iconButton, reportButton, accomplishmentButton, monitorButton;
 
     public static final String DEFAULT = "not available";
 
@@ -28,35 +32,44 @@ public class HomePage extends AppCompatActivity {
         String currentSessionUsername = prefs.getString( "CurrentSessionUsername", DEFAULT );
 
         usernameTextView.setText(currentSessionUsername);
-    }
 
+        iconButton = findViewById(R.id.iconButton);
+        iconButton.setOnClickListener(this);
+        findStationButton = findViewById(R.id.findStationButton);
+        findStationButton.setOnClickListener(this);
+        reportButton = findViewById(R.id.reportButton);
+        reportButton.setOnClickListener(this);
+        accomplishmentButton = findViewById(R.id.accomplishmentButton);
+        accomplishmentButton.setOnClickListener(this);
+        monitorButton = findViewById(R.id.monitorButton);
+        monitorButton.setOnClickListener(this);
 
-    public void Search(View view){
-        Intent i = new Intent( this, ReportChoosing.class );
-        startActivity( i );
-    }
-
-    public void enterReport(View view){
-        Intent i = new Intent( this, ReportActivity.class );
-        startActivity( i );
-        Toast.makeText( this, "Entering Report page", Toast.LENGTH_LONG ).show();
-
-    }
-
-    public void enterAchieve(View view){
-        Intent i = new Intent( this, AchieveActivity.class );
-        startActivity( i );
-        Toast.makeText( this, "Entering Achievement page", Toast.LENGTH_LONG ).show();
 
     }
 
-    public void enterProfile(View view){
-        Intent i = new Intent( this, ProfileActivity.class );
-        startActivity( i );
-        Toast.makeText( this, "Entering User Profile Page", Toast.LENGTH_LONG ).show();
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.iconButton) {
+            Intent i = new Intent( this, ProfileActivity.class );
+            startActivity( i );
+        }
 
+        if (v.getId() == R.id.findStationButton) {
+
+        }
+
+        if (v.getId() == R.id.reportButton) {
+            Intent i = new Intent( this, ReportActivity.class );
+            startActivity( i );
+        }
+
+        if (v.getId() == R.id.accomplishmentButton) {
+            Intent i = new Intent( this, AchieveActivity.class );
+            startActivity( i );
+        }
+
+        if (v.getId() == R.id.monitorButton) {
+
+        }
     }
-
-
-
 }
---
+++ app/src/main/java/com/example/milestone2/WalkingSpeedActivity.java
@@ -0,0 +1,14 @@
+package com.example.milestone2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WalkingSpeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walking_speed);
+    }
+}
